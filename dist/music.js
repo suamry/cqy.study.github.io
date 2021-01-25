a90b5883598da96198dcec24393a92a7e4f26886
Sua-CAY <2551165612@qq.com>
2021-01-25 15:46:17 +0800

    Site updated: 2021-01-25 15:46:16

---
+++ dist/music.js
@@ -0,0 +1,44 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    volume: 0.3,
+    loop: 'all',
+    audio: [
+      {
+        name: "我爱他",
+        artist: '陈威全',
+        url: 'http://m10.music.126.net/20200225140251/9176223a9bf7de7029403d9b9774128d/ymusic/6458/c9ab/4bf6/8d053f688e51601dd76d8db29fdc8f96.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20171117/20171117142652315559.jpg',
+      },
+      {
+        name: '红昭愿',
+        artist: '音阙诗听',
+        url: 'http://m10.music.126.net/20200225140038/39141e0381c1a4e7e774ec3a34ba15af/ymusic/a201/7d78/e420/e12da94e22a825f422ddebdfc50e8520.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20170407/20170407225248906484.jpg',
+      },
+      {
+        name: '婆婆打嘞',
+        artist: '小君',
+        url: 'http://m10.music.126.net/20200225140000/4b3462703e5d32ebe886308bab59ce23/ymusic/43bc/9a7f/a2cc/66e36637a99a0dfbf73b1c5668ee513f.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20130625/20130625181709936236.jpg',
+      },
+      {
+        name: '慢慢喜欢你',
+        artist: '莫文蔚',
+        url: 'http://m10.music.126.net/20200225140349/053c2cc7bff50ffbad5d752953aa8f4f/ymusic/036c/9f6b/412f/f574a4e6f9d53e030d9394f77dc73b09.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20130625/20130625181709936236.jpg',
+      },
+      {
+        name: '爱情转移',
+        artist: '陈奕迅',
+        url: 'http://m10.music.126.net/20200225135612/a07826e1beba7b9fcc8a184996dac5c6/ymusic/6811/1264/ea16/1d4423658007def2c642a5b18c311fdf.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/20161010/20161010200518926406.jpg',
+      }
+    ]
+});
+
+
+
+
+
